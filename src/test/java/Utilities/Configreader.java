584b465979aea9a2ca56c525a6c714dc36ec0cd6
Undala Venu <undalavenu17@gmail.com>
2026-09-23 11:18:42 +0530

    updated Frame workwith Proprrties Path

--- src/test/java/Utilities/Configreader.java
+++ src/test/java/Utilities/Configreader.java
@@ -2,6 +2,7 @@ package Utilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Configreader {
@@ -11,7 +12,9 @@ public class Configreader {
 			
 		
 		pr= new Properties();
-		FileInputStream fil= new FileInputStream("C:\\Users\\TESTING\\eclipse-workspace\\Pagefactory1\\Properties\\Config.properties");
+	    InputStream fil = getClass().getClassLoader().getResourceAsStream("Properties/Config.properties");
+	    
+	//	FileInputStream fil= new FileInputStream("C:\\Users\\TESTING\\eclipse-workspace\\Pagefactory1\\Properties\\Config.properties");
 		pr.load(fil);
 		}
 		catch(Exception e) {
